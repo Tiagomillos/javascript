e2bc07fe6286a647b3d6b403af6fb24c7ed1fe43
Tiago Pereira <tpmillos0410@gmail.com>
2025-08-31 08:36:36 -0300

    minhas att em js

---
+++ aula4/ex009.js
@@ -0,0 +1,10 @@
+var smartphone = 'Nokia'
+console.log(`Meu smartphone é da marca ${smartphone}`)
+if (smartphone == 'Aplle') {
+    console.log('Você tem as melhores condições de status')
+}
+
+else { (smartphone == 'Samsung') 
+    console.log('Você tem um smartphone de melhor custo-benefício')
+
+}
