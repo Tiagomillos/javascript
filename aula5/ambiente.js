3d2ec54e9763ad0a708d4556a638d807a5b525e3
Tiago Pereira <tpmillos0410@gmail.com>
2025-09-25 15:19:22 -0300

    alterações

---
+++ aula5/ambiente.js
@@ -0,0 +1,17 @@
+var c = 1
+while (c <= 6) {
+    console.log('Olá!')
+    c++ // c = c + 1
+}
+
+
+
+/*
+console.log('Olá!')
+console.log('Olá!')
+console.log('Olá!')
+console.log('Olá!')
+console.log('Olá!')
+console.log('Olá!')
+console.log('Olá!')
+*/
